5bbbf2b0afbeb44e45a936328fbf481a6ba5997e
Shravani <shrau.3003@gmail.com>
2026-09-15 18:39:45 +0530

    Time: 24 ms (50.12%), Space: 49.6 MB (59.65%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -8,7 +8,40 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
+    public int[] nextLargerNodes(ListNode head) {
+
+        ArrayList<Integer> values = new ArrayList<>();
+        ListNode ptr = head;
+
+        while (ptr != null) {
+            values.add(ptr.val);
+            ptr = ptr.next;
+        }
+
+        int[] ans = new int[values.size()];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < values.size(); i++) {
+
+            while (!stack.isEmpty() &&
+                   values.get(stack.peek()) < values.get(i)) {
+
+                int index = stack.pop();
+                ans[index] = values.get(i);
+            }
+
+            stack.push(i);
+        }
+
+        return ans;
+    }
+}
+
+
+/*class Solution {
     public int[] nextLargerNodes(ListNode head) {
 
     ArrayList<Integer> list = new ArrayList<>();
@@ -18,7 +51,7 @@ class Solution {
     {
         ListNode ptr = temp.next;
         boolean found=false;
-        
+
         while(ptr!=null)
         {
             if(ptr.val>temp.val)
@@ -44,4 +77,4 @@ class Solution {
     }
     return arr;   
     }
-}
+}*/
